eacc0d876f4c633f3a8b8ba2b375a662b2ac0175
302403124 <302403124@student.rocmondriaan.nl>
2019-11-22 13:06:56 +0100

    commit voor vrijdag

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -1,24 +1,36 @@
 <?php
+
 namespace App\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+use App\Entity\Training;
+use Symfony\Component\Routing\Annotatio\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-class ArticleController
+
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @\Sensio\Bundle\FrameworkExtraBundle\Configuration\Route("/training", name="create_product")
      */
-    public function homepage()
+    public function createProduct(): Response
     {
-        return new Response('Test123');
-    }
-    /**
-     * @Route("/{slug}")
-     */
-    public function show($slug)
-    {
-        return new Response(sprintf(
-            'Het volgende: "%s"',
-            $slug
-        ));
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $training = new training();
+        $training->setNaam('Kickboks');
+        $training->setDescription('conditie training en oefenen met kicks');
+        $training->setDuration(45);
+        $training->setCosts(25);
+
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($training);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new training with id '.$training->getId());
     }
 }
